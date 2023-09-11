8c3188a507027ed0be32c3e5476ae1d4088f86db
mabe447c <marcel.beyer2@mailbox.tu-dresden.de>
2023-09-11 16:43:15 +0200

    Created new ga version

---
+++ cmake-build-debug/ga.h
@@ -0,0 +1,14 @@
+//
+// Created by marce on 11.09.2023.
+//
+
+#ifndef GENETIC_ALGORITHM_VLSI_V2_GA_H
+#define GENETIC_ALGORITHM_VLSI_V2_GA_H
+
+
+class ga {
+
+};
+
+
+#endif //GENETIC_ALGORITHM_VLSI_V2_GA_H
